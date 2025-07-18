873c9a89ea3f0743be03620d20cf087f056d0f04
manasa-govada <manasagovada52@gmail.com>
2025-07-18 11:55:26 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,12 @@
+package com.sample.test.coreprograms;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		System.out.println("The sum of the:" +a +"and" +b+"=" +(a+b));
+
+	}
+
+}
+
